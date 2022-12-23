0514026f69e7c8b769edfa64ea83c45a488f99aa
RekhuGopal <42715745+RekhuGopal@users.noreply.github.com>
2022-12-23 17:15:18 +0530

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -55,7 +55,7 @@ Write-Output '*****Git fetch origin****'
 git fetch $sourceURL
 Write-Output '*****Git push secondary****'
 #git remote set-url origin $destURLSetURL
-git push secondary  --all
+git push secondary  --all -f
 Write-Output '**Azure Devops repo synced with Github repo**'
 Set-Location $stageDir
 if((Test-Path -path $githubDir))
